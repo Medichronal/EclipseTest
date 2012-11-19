f3bbbaf2a27791d347e280c3e7929108a0faf21a
Medichronal <medichronal@gmail.com>
2012-11-19 20:40:53 +0200

    Adding TestClass1.

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,5 +2,11 @@
 public class HelloWorld {
 	public static void main(String args[]) {
 		System.out.println("HelloWorld!");
+		
+		TestClass1 obj = new TestClass1(1);
+		obj.setIndex(5);
+		
+		System.out.println(obj.getIndex());
+		
 	}
 }
---
+++ HelloWorld/src/TestClass1.java
@@ -0,0 +1,17 @@
+
+public class TestClass1 {
+	private int index;
+	
+	public int getIndex() {
+		return index;
+	}
+	
+	public void setIndex(int i) {
+		index = i;
+	}
+	
+	public TestClass1(int i) {
+		setIndex(i);
+	}
+	
+}
